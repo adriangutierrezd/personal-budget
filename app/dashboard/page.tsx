b37589e2fe0d3d2719563bd2ac8c7c1ad7eef355
adriangutierrezd <adriangutierrezdelaosa@gmail.com>
2024-02-26 06:48:21 +0100

    fix

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,17 +5,12 @@ import ExpensesTable from "../ui/dashboard/ExpensesTable";
 
 export default function Dashboard () {
 
-
-  const handleReRender = () => {
-
-  }
-
   return (
     <main className="p-6">
       <section className="max-w-7xl mx-auto">
         <h1 className="text-2xl font-bold mb-4">Panel de control</h1>
       </section>
-      <StatsCards handleReRender={handleReRender} /> 
+      <StatsCards /> 
       <ExpensesByCategory/>
       <ExpensesTable/>
     </main>
