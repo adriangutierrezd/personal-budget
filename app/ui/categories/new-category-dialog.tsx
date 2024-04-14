426113dc9c81f254055227deb259282faee0e599
adriangutierrezd <adriangutierrezdelaosa@gmail.com>
2024-04-14 20:39:49 +0200

    prueba de drawer

--- app/ui/categories/new-category-dialog.tsx
+++ app/ui/categories/new-category-dialog.tsx
@@ -1,14 +1,7 @@
-import { useState } from "react";
+"use client"
+import * as React from "react"
 import { z } from "zod"
 import { useToast } from "@/components/ui/use-toast"
-import {
-  AlertDialog,
-  AlertDialogContent,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog"
 import { categoryTypes } from "@/lib/constants"
 import {
   Select,
@@ -19,7 +12,6 @@ import {
 } from "@/components/ui/select"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
-import { Button } from "@/components/ui/button"
 import {
   Form,
   FormControl,
@@ -28,9 +20,28 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
 import { Session } from "next-auth";
 import { storeCategory } from "@/lib/services/categoriesService";
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer"
+import { Input } from "@/components/ui/input"
+import { AlertDialogFooter } from "@/components/ui/alert-dialog";
 
 
 const categoryForm = z.object({
@@ -41,43 +52,45 @@ const categoryForm = z.object({
   color: z.string().min(6).max(10),
 })
 
-interface Props{
-  readonly userData: Session|undefined;
+const formDefaultValues = {
+  name: '',
+  type: 'EXPENSES',
+  color: '#000000'
+}
+
+interface Props {
+  readonly userData: Session | undefined;
   readonly reload: () => void
 }
 
-export default function NewCategoryDialog({userData, reload}: Props) {
+export default function NewCategoryDialog({ userData, reload }: Props) {
 
-  const [isEditDialogOpen, setIsEditDialogOpen] = useState<boolean>(false)
-  const {toast} = useToast()
+  const [open, setOpen] = React.useState(false)
+  const [openDesktop, setOpenDesktop] = React.useState(false)
+  const { toast } = useToast()
 
   const newCategoryForm = useForm<z.infer<typeof categoryForm>>({
     resolver: zodResolver(categoryForm),
-    defaultValues: {
-      name: '',
-      type: 'EXPENSES',
-      color: '#000000'
-    },
+    defaultValues: formDefaultValues,
   })
 
-  const {reset} = newCategoryForm
+  const { reset } = newCategoryForm
 
 
   // @ts-ignore
   const onSubmit = async (values: z.infer<typeof categoryForm>) => {
 
-    try{
+    try {
 
-      if(!userData) throw new Error('Ha ocurrido un error de autenticación');
+      if (!userData) throw new Error('Ha ocurrido un error de autenticación');
 
       await storeCategory({
         token: userData.user.token,
         props: values
       })
       reload()
-      setIsEditDialogOpen(false)
       reset({})
-    }catch(error: any){
+    } catch (error: any) {
       toast({
         variant: "destructive",
         title: "Ha ocurrido un error",
@@ -86,81 +99,119 @@ export default function NewCategoryDialog({userData, reload}: Props) {
     }
 
   }
-    return (
-      <AlertDialog open={isEditDialogOpen} onOpenChange={() => setIsEditDialogOpen(!isEditDialogOpen)}>
-        <AlertDialogTrigger asChild>
-           <Button type="button" variant="outline">
-        Añadir categoría
-      </Button>
-        </AlertDialogTrigger>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>Detalles de la categoría</AlertDialogTitle>
-          </AlertDialogHeader>
-          <Form {...newCategoryForm}>
-            <form onSubmit={newCategoryForm.handleSubmit(onSubmit)} className="grid grid-cols-2 gap-4">
-              <FormField
-                control={newCategoryForm.control}
-                name="name"
-                render={({ field }) => (
-                  <FormItem className="col-span-2">
-                    <FormLabel>Nombre *</FormLabel>
-                    <FormControl>
-                      <Input {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
 
-<FormField
-                control={newCategoryForm.control}
-                name="type"
-                render={({ field }) => (
-                  <FormItem className="col-span-2">
-                    <FormLabel>Categoría *</FormLabel>
-                    <Select onValueChange={field.onChange} defaultValue={field.value}>
-                      <FormControl>
-                        <SelectTrigger>
-                          <SelectValue placeholder="Selecciona una categoría" />
-                        </SelectTrigger>
-                      </FormControl>
-                      <SelectContent>
-                        {categoryTypes.map((type) => {
-                          return (
-                            <SelectItem key={type.value} value={type.value}>
-                              {type.text}
-                            </SelectItem>
-                          )
-                        })}
-                      </SelectContent>
-                    </Select>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+  return (
+    <>
+      <div className="block md:hidden">
+        <Drawer open={open} onOpenChange={setOpen}>
+          <DrawerTrigger asChild>
+            <Button variant="outline">Añadir categoría</Button>
+          </DrawerTrigger>
+          <DrawerContent>
+            <DrawerHeader className="text-left">
+              <DrawerTitle>Detalles de la categoría</DrawerTitle>
+            </DrawerHeader>
+            <CategoriesForm handleSubmit={onSubmit} className="px-4" />
+            <DrawerFooter className="pt-2">
+              <DrawerClose asChild>
+                <Button variant="outline">Cancelar</Button>
+              </DrawerClose>
+            </DrawerFooter>
+          </DrawerContent>
+        </Drawer>
+      </div>
+      <div className="hidden md:block">
+        <Dialog open={openDesktop} onOpenChange={setOpenDesktop}>
+          <DialogTrigger asChild>
+            <Button variant="outline">Añadir categoría</Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Detalles de la categoría</DialogTitle>
+            </DialogHeader>
+            <CategoriesForm handleSubmit={onSubmit} />
+          </DialogContent>
+        </Dialog>
+      </div>
+    </>
+  )
+}
 
-              <FormField
-                control={newCategoryForm.control}
-                name="color"
-                render={({ field }) => (
-                  <FormItem className="col-span-2">
-                    <FormLabel>Color *</FormLabel>
-                    <FormControl>
-                      <Input type="color" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+interface CategoriesFormProps {
+  readonly className?: string;
+  readonly handleSubmit: (values: z.infer<typeof categoryForm>) => void;
+}
 
-              <AlertDialogFooter className="col-span-2">
-                <Button onClick={() => setIsEditDialogOpen(false)} type="button" variant="outline">Cancelar</Button>
-                <Button type="submit">Guardar</Button>
-              </AlertDialogFooter>
-            </form>
-          </Form>
-        </AlertDialogContent>
-      </AlertDialog>
-      )
+function CategoriesForm({ className, handleSubmit }: CategoriesFormProps) {
+
+  const newCategoryForm = useForm<z.infer<typeof categoryForm>>({
+    resolver: zodResolver(categoryForm),
+    defaultValues: formDefaultValues,
+  })
+
+  return (
+
+    <Form {...newCategoryForm}>
+      <form onSubmit={newCategoryForm.handleSubmit(handleSubmit)} className={cn("grid grid-cols-2 gap-4", className)}>
+        <FormField
+          control={newCategoryForm.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem className="col-span-2">
+              <FormLabel>Nombre *</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={newCategoryForm.control}
+          name="type"
+          render={({ field }) => (
+            <FormItem className="col-span-2">
+              <FormLabel>Categoría *</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Selecciona una categoría" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  {categoryTypes.map((type) => {
+                    return (
+                      <SelectItem key={type.value} value={type.value}>
+                        {type.text}
+                      </SelectItem>
+                    )
+                  })}
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={newCategoryForm.control}
+          name="color"
+          render={({ field }) => (
+            <FormItem className="col-span-2">
+              <FormLabel>Color *</FormLabel>
+              <FormControl>
+                <Input type="color" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <AlertDialogFooter className="col-span-2">
+          <Button className="w-full" type="submit">Guardar</Button>
+        </AlertDialogFooter>
+      </form>
+    </Form>
+  )
 }
